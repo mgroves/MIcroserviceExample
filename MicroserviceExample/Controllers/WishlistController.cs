12ef1016dc2636ec8be58dbaf7d8b65601dc9ad4
Matthew D. Groves <me@mgroves.com>
2020-07-22 14:34:46 -0400

    initial commit of a very simple microservice

---
+++ MicroserviceExample/Controllers/WishlistController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Couchbase.Extensions.DependencyInjection;
+using Couchbase.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceExample.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IBucketProvider _bucketProvider;
+
+        public EventController(IBucketProvider bucketProvider)
+        {
+            _bucketProvider = bucketProvider;
+        }
+
+        [HttpPost]
+        [Route("/event/add{userId}")]
+        public async Task<IActionResult> AddEventToUser(string userId, UserEventPost evt)
+        {
+            var bucket = await _bucketProvider.GetBucketAsync("useractivity");
+            var coll = bucket.DefaultCollection();
+            
+            var activityId = $"activity::{Guid.NewGuid()}";
+            await coll.InsertAsync(activityId, new
+            {
+                userId,
+                evt.Description,
+                evt.EventType,
+                EventDt = DateTime.Now
+            });
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("/activity/get/{userId}")]
+        public async Task<IActionResult> GetEventsForUser(string userId)
+        {
+            // CREATE INDEX ix_userid on useractivity (userId);
+            var bucket = await _bucketProvider.GetBucketAsync("useractivity");
+            var cluster = bucket.Cluster;
+            var events = await cluster.QueryAsync<UserEventGet>(
+                @"SELECT description, eventType, eventDt
+                            FROM useractivity
+                            WHERE userId = $userId
+                            ORDER BY eventDt DESC",
+                QueryOptions.Create().Parameter("$userId", userId));
+            return Ok(events.Rows);
+        }
+    }
+
+    public class UserEventPost
+    {
+        public string Description { get; set; }
+        public string EventType { get; set; }
+    }
+
+    public class UserEventGet
+    {
+        public string Description { get; set; }
+        public string EventType { get; set; }
+        public DateTime EventDt { get; set; }
+    }
+}
